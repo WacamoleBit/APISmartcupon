9c2a1148ad60469fc46ebaf6657e05f45a05b407
Joyce <zs19016410@estudiantes.uv.mx>
2023-11-30 13:45:08 -0600

    ws empresa

---
+++ src/java/modelo/EmpresaDAO.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import modelo.pojo.Empresa;
+import modelo.pojo.Mensaje;
+import mybatis.MyBatisUtil;
+import org.apache.ibatis.session.SqlSession;
+
+/**
+ *
+ * @author jegal
+ */
+public class EmpresaDAO {
+    
+    public static Mensaje registrarEmpresa(Empresa empresa){
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(true);
+        SqlSession conexionDB = MyBatisUtil.getSession();
+        if (conexionDB != null){
+            try{
+                int numeroFilasAfectadas = conexionDB.insert("empresa.resgistrarEmpresa", empresa);
+                conexionDB.commit();
+                
+                if(numeroFilasAfectadas > 0){
+                    mensaje.setError(false);
+                    mensaje.setMensaje("Empresa registrada con éxito.");
+                }else{
+                    mensaje.setMensaje("Error: No se pudo registrar la empresa, por favor intenta de nuevo.");
+                }
+            }catch(Exception e){
+                mensaje.setMensaje("Error" + e.getMessage());
+            }finally{
+                conexionDB.close();
+            }
+        }else{
+            mensaje.setMensaje("Error: Por el momento no hay conexión con la base de datos, intenta mas tarde.");
+        }
+        return  mensaje;
+    }
+
+    public static Mensaje editarEmpresa(Empresa empresa){
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(true);
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if(conexionBD != null){
+            try{
+                int numeroFilasAfectadas = conexionBD.update("empresa.editarEmpresa", empresa);
+                conexionBD.commit();
+                if(numeroFilasAfectadas > 0){
+                mensaje.setError(false);
+                mensaje.setMensaje("Empresa modificada con éxito.");
+                }else{
+                    mensaje.setMensaje("Error: No se pudo modificar la empresa, por favor intenta de nuevo.");
+                }
+            }catch(Exception e){
+                mensaje.setMensaje("Error" + e.getMessage());
+            }finally{
+                conexionBD.close();
+            }
+        }else{
+            mensaje.setMensaje("Error: Por el momento no hay conexión con la base de datos, intenta mas tarde.");
+        }
+        return mensaje;
+    }
+    
+    public static Mensaje eliminarEmpresa(Empresa empresa){
+        Mensaje mensaje = new Mensaje();
+        mensaje.setError(true);
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if(conexionBD != null){
+            try{
+                int numeroFilasAfectadas = conexionBD.delete("empresa.emilinarEmpresa", empresa);
+                if(numeroFilasAfectadas > 0){
+                    mensaje.setError(false);
+                    mensaje.setMensaje("Empresa eliminada con éxito.");
+                }else{
+                    mensaje.setMensaje("Error: No se pudo eliminar la empresa, por favor intenta de nuevo.");
+                }
+            }catch(Exception e){
+                mensaje.setMensaje("Error" + e.getMessage());
+            }finally{
+                conexionBD.close();
+            }
+        }else{
+            mensaje.setMensaje("Error: Por el momento no hay conexión con la base de datos, intenta mas tarde.");
+        }
+        return mensaje;
+    }
+}
