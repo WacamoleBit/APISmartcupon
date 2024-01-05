b4aa06be4dd99e6b920608650590442cdd88d146
Manuel Hernandez <mahmaruro@outlook.com>
2024-01-05 02:01:29 -0600

    mandar mas datos en solicitudes de sucursal

--- src/java/modelo/pojo/Sucursal.java
+++ src/java/modelo/pojo/Sucursal.java
@@ -14,32 +14,30 @@ public class Sucursal {
     private Integer idSucursal;
     private String nombre;
     private Integer direccion;
+    private String direccionNombre;
     private String telefono;
     private Double latitud;
     private Double longitud;
     private Integer encargado;
+    private String encargadoNombre;
     private Integer empresa;
+    private String empresaNombre;
 
-    public Sucursal(Integer idSucursal, String nombre, Integer direccion, String telefono, Double latitud, Double longitud, Integer encargado, Integer empresa) {
+    public Sucursal() {
+    }
+
+    public Sucursal(Integer idSucursal, String nombre, Integer direccion, String direccionNombre, String telefono, Double latitud, Double longitud, Integer encargado, String encargadoNombre, Integer empresa, String empresaNombre) {
         this.idSucursal = idSucursal;
         this.nombre = nombre;
         this.direccion = direccion;
+        this.direccionNombre = direccionNombre;
         this.telefono = telefono;
         this.latitud = latitud;
         this.longitud = longitud;
         this.encargado = encargado;
+        this.encargadoNombre = encargadoNombre;
         this.empresa = empresa;
-    }
-
-    public Integer getEmpresa() {
-        return empresa;
-    }
-
-    public void setEmpresa(Integer empresa) {
-        this.empresa = empresa;
-    }
-
-    public Sucursal() {
+        this.empresaNombre = empresaNombre;
     }
 
     public Integer getIdSucursal() {
@@ -64,6 +62,14 @@ public class Sucursal {
 
     public void setDireccion(Integer direccion) {
         this.direccion = direccion;
+    }
+
+    public String getDireccionNombre() {
+        return direccionNombre;
+    }
+
+    public void setDireccionNombre(String direccionNombre) {
+        this.direccionNombre = direccionNombre;
     }
 
     public String getTelefono() {
@@ -96,6 +102,30 @@ public class Sucursal {
 
     public void setEncargado(Integer encargado) {
         this.encargado = encargado;
+    }
+
+    public String getEncargadoNombre() {
+        return encargadoNombre;
+    }
+
+    public void setEncargadoNombre(String encargadoNombre) {
+        this.encargadoNombre = encargadoNombre;
+    }
+
+    public Integer getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Integer empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getEmpresaNombre() {
+        return empresaNombre;
+    }
+
+    public void setEmpresaNombre(String empresaNombre) {
+        this.empresaNombre = empresaNombre;
     }
 
 }
